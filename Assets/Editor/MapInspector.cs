2b329b346fc10062fef1f3d204c003dee2647176
nevaermorr <pi.wiacek@gmail.com>
2014-10-02 23:47:08 +0200

    init

---
+++ Assets/Editor/MapInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Map))]
+public class MapInspector : Editor {
+	
+	public override void OnInspectorGUI() {
+		//base.OnInspectorGUI();
+		DrawDefaultInspector();
+		if (GUILayout.Button("Regenerate")) {
+			Map map = (Map) target;
+//			tileMap.BuildMesh();
+		}
+	}
+}
