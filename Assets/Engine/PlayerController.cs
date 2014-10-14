37af1f5675f5deb18c4eee80096034dc82a99a10
nevaermorr <pi.wiacek@gmail.com>
2014-10-14 22:53:28 +0200

    moving along the grid with collisions

--- Assets/Engine/PlayerController.cs
+++ Assets/Engine/PlayerController.cs
@@ -1,78 +1,85 @@
-using System.Collections;
 using UnityEngine;
- 
-class PlayerController: MonoBehaviour {
-    private float moveSpeed = 10f;
-    
-    private Vector3 destination;
-    private float movePoint;
-    
-    private Vector2 input;
-    private bool isMoving = false;
-    
-    public void Update() {
-    	if (!isMoving){
-			Move();
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 6.0F;
+	private Vector3 moveDestination = Vector3.zero;
+	private Vector3 startingPoint;
+	private Vector3 requestedDirection;
+	private bool isMoving = false;
+	
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		// If not moving - give chance to move
+		if (!isMoving) {
+			requestedDirection = GetMoveDirection();
+			// Start movement
+			if (requestedDirection != Vector3.zero) {
+				isMoving = true;
+				// Note the starting point in case way is blocked and return is needed
+				startingPoint = transform.position;
+				moveDestination = transform.position + requestedDirection;
+			}
 		}
-    }
+		// If the entity is moving
+		if (isMoving) {
+			// If it is close enough to its destination - snap to the grid
+			if (Vector3.Distance(moveDestination, transform.position) < 0.1f) {
+				transform.position = moveDestination;
+				// Stop moving
+				isMoving = false;
+			}
+			// Move toward destination
+			else {
+				Vector3 moveDirection = transform.TransformDirection((moveDestination - transform.position).normalized);
+				moveDirection *= speed;
+				controller.Move(moveDirection * Time.deltaTime);
+			}
+		}
+	}
     
-    private void Move() {
-    	Vector2 direction = GetMoveDirection();
-    	// If there is a movement
-    	if (direction != Vector2.zero) {
-    		destination = transform.position + new Vector3(direction.x, 0f, direction.y);
-    		StartCoroutine(move(transform));
-    	}
-    }
-    
-    private Vector2 GetMoveDirection() {
-    	Vector2 direction;
-    	if 		(Input.GetButton("move SW")) {
-    		direction = new Vector2(-1f, -1f);
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+		// If encountered some obstacle, return to the starting point
+		if (body == null) {
+			// Return to starting position
+			transform.position = startingPoint;
+			// End movement
+			isMoving = false;
+		}
+	}
+	
+	private Vector3 GetMoveDirection() {
+		Vector3 direction;
+		if 		(Input.GetButton("move SW")) {
+			direction = new Vector3(-1f, 0f, -1f);
 		}
 		else if (Input.GetButton("move S")) {
-			direction = new Vector2(0f, -1f);
+			direction = new Vector3(0f, 0f, -1f);
 		}
 		else if (Input.GetButton("move SE")) {
-			direction = new Vector2(1f, -1f);
+			direction = new Vector3(1f, 0f, -1f);
 		}
 		else if (Input.GetButton("move W")) {
-			direction = new Vector2(-1f, 0f);
+			direction = new Vector3(-1f, 0f, 0f);
 		}
 		else if (Input.GetButton("move E")) {
-			direction = new Vector2(1f, 0f);
+			direction = new Vector3(1f, 0f, 0f);
 		}
 		else if (Input.GetButton("move NW")) {
-			direction = new Vector2(-1f, 1f);
+			direction = new Vector3(-1f, 0f, 1f);
 		}
 		else if (Input.GetButton("move N")) {
-			direction = new Vector2(0f, 1f);
+			direction = new Vector3(0f, 0f, 1f);
 		}
 		else if (Input.GetButton("move NE")) {
-			direction = new Vector2(1f, 1f);
+			direction = new Vector3(1f, 0f, 1f);
 		}
 		else {
-			direction = new Vector2(0f, 0f);
+			direction = Vector3.zero;
 		}
 		
 		return direction;
     }
-    
-    public IEnumerator move(Transform transform) {
-    	// Start moving
-        isMoving = true;
-        
-        // While destination is still away
-        while (Vector3.Distance(transform.position, destination) > 0.1f) {
-        	transform.position += (destination - transform.position).normalized * moveSpeed * Time.deltaTime;
-        	yield return null;
-        }
-        // Once we are close enough
-        transform.position = destination;
-        // Stop moving
-        isMoving = false;
-        
-        yield return true;
-     }
 }
-	
